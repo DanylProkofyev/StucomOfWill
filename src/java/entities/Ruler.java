66575d1a8d7707f5890df67172d7bd662bae4fef
X2382383C <X2382383C@A26O30.novaulas.stucom.com>
2017-04-26 17:27:34 +0200

    ruler creado

--- src/java/entities/Ruler.java
+++ src/java/entities/Ruler.java
@@ -5,10 +5,75 @@
  */
 package entities;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
 /**
  *
  * @author X6095888Z
  */
+
+@Entity
+@Table(name = "batalla")
+@XmlRootElement
+@NamedQueries({
+@NamedQuery(name = "Ruler.findAll", query = "SELECT b FROM Ruler b")})
+
+
 public class Ruler {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idruler")
+    private Integer idruler;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "efecto")
+    private String efecto;
+
+
+
+    public Ruler(){
+    }
+
+    public Integer getIdruler() {
+        return idruler;
+    }
+
+    public void setIdruler(Integer idruler) {
+        this.idruler = idruler;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEfecto() {
+        return efecto;
+    }
+
+    public void setEfecto(String efecto) {
+        this.efecto = efecto;
+    }
+    
+    
     
 }
