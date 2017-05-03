c860dd0380587f11da16775ee3891328ee10c044
danylprokofyev <danyl01000100@gmail.com>
2017-05-03 17:00:00 +0200

    usuario++

--- src/java/entities/Usuario.java
+++ src/java/entities/Usuario.java
@@ -6,6 +6,7 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -27,7 +29,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Table(name = "usuario")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")})
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+@NamedQuery(name = "Usuario.findByPuntos", query = "SELECT u FROM Usuario u"),
+@NamedQuery(name = "Trainer.findAllOrder", query = "SELECT u FROM Usuario u order by u.puntos desc")})
 
 public class Usuario implements Serializable {
 
@@ -42,9 +46,76 @@ public class Usuario implements Serializable {
     private String contra;
     @Basic(optional = false)
     @NotNull
-    @Column(name = "points")
-    private int points;
+    @Column(name = "puntos")
+    private int puntos;
     @JoinColumn(name = "ruler", referencedColumnName = "idruler")
     private int ruler;
+    @OneToMany(mappedBy = "usuario")
+    private Collection<Cartas> coleccionCartas;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getRuler() {
+        return ruler;
+    }
+
+    public void setRuler(int ruler) {
+        this.ruler = ruler;
+    }
+
+    public Collection<Cartas> getColeccionCartas() {
+        return coleccionCartas;
+    }
+
+    public void setColeccionCartas(Collection<Cartas> coleccionCartas) {
+        this.coleccionCartas = coleccionCartas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) obj;
+        if ((this.nombre == null && other.nombre != null) || (this.nombre != null && !this.nombre.equals(other.nombre))) {
+            return false;
+        }
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
