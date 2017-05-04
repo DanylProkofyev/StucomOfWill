bd4b5e15b89fa93b704d5fe19ceff6d91b1c7433
kelvinlt <kelvinleethen@gmail.com>
2017-05-04 20:04:47 +0200

    Hecho carta y dos cosas del beans

--- src/java/beans/WillEJB.java
+++ src/java/beans/WillEJB.java
@@ -5,7 +5,14 @@
  */
 package beans;
 
+import entities.Usuario;
+import entities.Batalla;
+import entities.Ruler;
+import entities.Cartas;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
 /**
  *
@@ -16,4 +23,25 @@ public class WillEJB {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    public boolean insertarUsuario(Usuario t) {
+        if (!existeUsuario(t)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(t);
+            em.close();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existeUsuario(Usuario t) {
+        EntityManager em = emf.createEntityManager();
+        Usuario usuarioEncontrado = em.find(Usuario.class, t.getNombre());
+        em.close();
+        return usuarioEncontrado != null;
+    }
+    
+    
 }
